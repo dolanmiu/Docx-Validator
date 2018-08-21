41741abc2757069248c71eb561fa517b58d2fdc5
Dolan <dolan_miu@hotmail.com>
2018-08-21 23:15:05 +0100

    Initial Commit

---
+++ DocxValidatorV1/Validator.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Validation;
+using DocumentFormat.OpenXml.Wordprocessing;
+
+namespace DocxValidatorV1
+{
+    public static class Validator
+    {
+        [FunctionName("Validate")]
+        public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)]HttpRequestMessage req, TraceWriter log)
+        {
+            log.Info("C# HTTP trigger function processed a request.");
+
+            var body = await req.Content.ReadAsStringAsync();
+
+            CloudBlockBlob blob;
+            string name;
+
+            name = Guid.NewGuid().ToString("n");
+
+            string filepath = @"C:\Users\Public\Documents\Word18.docx";
+            ValidateWordDocument(filepath);
+            Console.WriteLine("The file is valid so far.");
+            Console.WriteLine("Inserting some text into the body that would cause Schema error");
+            Console.ReadKey();
+
+            ValidateCorruptedWordDocument(filepath);
+            Console.WriteLine("All done! Press a key.");
+            Console.ReadKey();
+
+
+            using (Stream stream = new MemoryStream(Encoding.UTF8.GetBytes(body)))
+            {
+                await blob.UploadFromStreamAsync(stream);
+            }
+
+            return req.CreateResponse(HttpStatusCode.OK, body);
+
+            /*
+            // parse query parameter
+            string name = req.Get()
+                .FirstOrDefault(q => string.Compare(q.Key, "name", true) == 0)
+                .Value;
+
+            if (name == null)
+            {
+                // Get request body
+                dynamic data = await req.Content.ReadAsAsync<object>();
+                name = data?.name;
+            }
+
+            return name == null
+                ? req.CreateResponse(HttpStatusCode.BadRequest, "Please pass a name on the query string or in the request body")
+                : req.CreateResponse(HttpStatusCode.OK, "Hello " + name);*/
+        }
+
+        public static void ValidateWordDocument(string filepath)
+        {
+            using (WordprocessingDocument wordprocessingDocument =
+                WordprocessingDocument.Open(filepath, true))
+            {
+                try
+                {
+                    OpenXmlValidator validator = new OpenXmlValidator();
+                    int count = 0;
+                    foreach (ValidationErrorInfo error in
+                        validator.Validate(wordprocessingDocument))
+                    {
+                        count++;
+                        Console.WriteLine("Error " + count);
+                        Console.WriteLine("Description: " + error.Description);
+                        Console.WriteLine("ErrorType: " + error.ErrorType);
+                        Console.WriteLine("Node: " + error.Node);
+                        Console.WriteLine("Path: " + error.Path.XPath);
+                        Console.WriteLine("Part: " + error.Part.Uri);
+                        Console.WriteLine("-------------------------------------------");
+                    }
+
+                    Console.WriteLine("count={0}", count);
+                }
+
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+                wordprocessingDocument.Close();
+            }
+        }
+
+        public static void ValidateCorruptedWordDocument(string filepath)
+        {
+            // Insert some text into the body, this would cause Schema Error
+            using (WordprocessingDocument wordprocessingDocument =
+                WordprocessingDocument.Open(filepath, true))
+            {
+                // Insert some text into the body, this would cause Schema Error
+                Body body = wordprocessingDocument.MainDocumentPart.Document.Body;
+                Run run = new Run(new Text("some text"));
+                body.Append(run);
+
+                try
+                {
+                    OpenXmlValidator validator = new OpenXmlValidator();
+                    int count = 0;
+                    foreach (ValidationErrorInfo error in
+                        validator.Validate(wordprocessingDocument))
+                    {
+                        count++;
+                        Console.WriteLine("Error " + count);
+                        Console.WriteLine("Description: " + error.Description);
+                        Console.WriteLine("ErrorType: " + error.ErrorType);
+                        Console.WriteLine("Node: " + error.Node);
+                        Console.WriteLine("Path: " + error.Path.XPath);
+                        Console.WriteLine("Part: " + error.Part.Uri);
+                        Console.WriteLine("-------------------------------------------");
+                    }
+
+                    Console.WriteLine("count={0}", count);
+                }
+
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+    }
+
+
+}
